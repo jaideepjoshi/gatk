224c97c7b736e94ed6b4d8b067ec830a9f8f2403
Steve Huang <shuang@broadinstitute.org>
2018-03-09 13:52:18 -0500

    set up test structure

---
+++ src/test/java/org/broadinstitute/hellbender/tools/spark/sv/discovery/inference/CpxVariantCanonicalRepresentationUnitTest.java
@@ -0,0 +1,7 @@
+package org.broadinstitute.hellbender.tools.spark.sv.discovery.inference;
+
+import org.broadinstitute.hellbender.GATKBaseTest;
+
+public class CpxVariantCanonicalRepresentationUnitTest extends GATKBaseTest {
+
+}
